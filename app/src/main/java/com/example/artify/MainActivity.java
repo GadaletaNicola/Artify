468e16c05d8401fc623d4646c4013cd288a5570a
Highwall-27 <mauropio.altamura@outlook.it>
2021-11-27 13:24:04 +0100

    Dashboard finita
    + dashboard
    + drawer
    + vari cambiamenti ai colori della status bar
    ~ da modificare tw e iw a login finito
    ~ da aggiungere hamburger button
    ~ aumentare raggio swipe per drawer

--- app/src/main/java/com/example/artify/MainActivity.java
+++ app/src/main/java/com/example/artify/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.artify;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this,DashboardActivity.class);
+        startActivity(i);
     }
+
+
 }
